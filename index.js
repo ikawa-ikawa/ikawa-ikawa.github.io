c980b2e81084268b7d0436e4a45298df12caf34b
Ikawa <55277915+ikawa-ikawa@users.noreply.github.com>
2020-06-02 14:16:23 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+function goTop()
+{
+  location.href = "index.html";
+}
+
+function goIntroduction()
+{
+  location.href = "introduction.html";
+}
+
+function goNews()
+{
+  location.href = "news.html";
+}
+
+function goCharacter()
+{
+  location.href = "character.html";
+}
+
+function goOnair()
+{
+  location.href = "onair.html";
+}
+
+function goTwitter()
+{
+  location.href = "https://twitter.com/5hlgb0QjJmjAdNa";
+}
+
+function changeImage( imgid , newimg )
+{
+  document.getElementById(imgid).src = newimg;
+}
+
+function changeText( textid , newtext )
+{
+  document.getElementById(textid).innerText = newtext;
+}
